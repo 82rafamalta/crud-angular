7084454e27e970db9d91a31b141496439eafff80
Rafa Malta <107279228+82rafamalta@users.noreply.github.com>
2023-01-10 17:50:04 -0300

    Add files via upload

---
+++ frontend/src/app/components/product/product-update/product-update.component.ts
@@ -0,0 +1,39 @@
+import { Product } from "./../product.model";
+import { ProductService } from "./../product.service";
+import { OnInit } from "@angular/core";
+import { Component } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+
+@Component({
+  selector: "app-product-update",
+  templateUrl: "./product-update.component.html",
+  styleUrls: ["./product-update.component.css"],
+})
+export class ProductUpdateComponent implements OnInit {
+  product!: Product;
+
+  constructor(
+    private productService: ProductService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get("id");
+    this.productService.readById(id).subscribe((product) => {
+      this.product = product;
+    });
+  }
+
+  updateProduct(): void {
+    this.productService.update(this.product).subscribe(() => {
+      this.productService.showMessage("Produto Alterado com Sucesso");
+      this.router.navigate(["/products"]);
+    });
+  }
+
+  cancelProduct(): void {
+    this.router.navigate(["/products"]);
+    this.productService.showMessage("Operação CANCELADA com Sucesso");
+  }
+}
